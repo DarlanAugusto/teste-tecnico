9362d87b000d9f41925a36a16cfbb4403966dd37
Darlan <darlansilv4@gmail.com>
2024-03-20 19:11:24 -0300

    views de erros http (not found, method not allowed)

---
+++ resources/views/errors/not-allowed.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>405 - Método Não Permitido</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+</head>
+    <body class="vh-100 d-flex flex-column">
+        <main class="container flex-grow-1 my-3 d-flex flex-column align-items-center justify-content-center">
+            <h1 style="font-size: 360px;">405</h1>
+            <h1 class="text-muted">Método não Permitido.</h1>
+            <div class="row">
+                <div class="col">
+                    <a href="/" class="btn btn-primary">Voltar para o Início</a>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
